64b2943aa1c9016710f5c54c9073288904d09cc7
tmddk220 <leeseunga1590@gmail.com>
2024-08-01 12:53:16 +0900

    dynamic path setting - URL Parameter & Query String

--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -1,5 +1,14 @@
+import { useParams } from "react-router-dom";
+
+// URL Parameter : / 뒤에 아이템의 id를 명시
+// (예) ~/product/id값
+// 아이템의 id 등의 변경되지 않는 값을 주소로 명시하기 위해 사용
+
 const Diary = () => {
-  return <div>Diary</div>;
+  const params = useParams();
+  console.log(params);
+
+  return <div>{params.id}번 일기입니다.</div>;
 };
 
 export default Diary;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import { useSearchParams } from "react-router-dom";
+
+// Query String : ? 뒤에 변수명과 값 명시
+// (예) ~/search?q=검색어
+// 검색어 등의 자주 변경되는 값을 주소로 명시하기 위해 사용
 
 const Home = () => {
+  const [params, setParams] = useSearchParams();
+  console.log(params.get("value"));
   return <div>Home</div>;
 };
 
