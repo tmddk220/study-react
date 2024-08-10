abf96c29e503c8dd5f8dabe277cf06dbd2a570b9
tmddk220 <leeseunga1590@gmail.com>
2024-08-10 15:00:22 +0900

    Diary  Page  Implementation

--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -1,14 +1,33 @@
-import { useParams } from "react-router-dom";
-
-// URL Parameter : / 뒤에 아이템의 id를 명시
-// (예) ~/product/id값
-// 아이템의 id 등의 변경되지 않는 값을 주소로 명시하기 위해 사용
+import { useParams, useNavigate } from "react-router-dom";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Viewer from "../components/Viewer";
+import useDiary from "../hooks/useDiary";
+import { getStringedDate } from "../util/get-stringed-date";
 
 const Diary = () => {
   const params = useParams();
-  console.log(params);
+  const nav = useNavigate();
+  const curDiaryItem = useDiary(params.id);
 
-  return <div>{params.id}번 일기입니다.</div>;
+  if (!curDiaryItem) {
+    return <div>데이터 로딩중...!</div>;
+  }
+
+  const { createdDate, emotionId, content } = curDiaryItem;
+  const title = getStringedDate(new Date(createdDate));
+  return (
+    <div>
+      <Header
+        title={`${title} 기록`}
+        leftchild={<Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />}
+        rightchild={
+          <Button onClick={() => nav(`/edit/${params.id}`)} text={"수정하기"} />
+        }
+      />
+      <Viewer emotionId={emotionId} content={content} />
+    </div>
+  );
 };
 
 export default Diary;
